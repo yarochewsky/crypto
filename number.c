e679b08f5bec54d774fe89c6ff97d5627ebd68f1
Daniel <yarochewsky@icloud.com>
2018-11-11 18:18:09 -0800

    removed macos temp files

--- number.c
+++ number.c
@@ -8,12 +8,8 @@ int32_t convert_base(int32_t target, uint8_t base_old, uint8_t base,
     uint32_t i = 0;
     uint32_t x = target;
     int32_t q = floor(x / base);
-    // length of new number
     uint32_t length = (log(target) / log(base)) + 1;
-    // allocate array for new number
     uint32_t* number = (uint32_t*) malloc(length);
-    // a0 a1 a2 ... a(length - 1) --> length numbers
-    // each number is a(length - 1 - i)
     *(number + (length - 1)) = x - q * base;
     while (q > 0) {
         i++;
